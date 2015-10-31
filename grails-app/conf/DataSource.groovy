4bb9f110b7848f22ea42e7c0965beed211440ef7
Ted Sanders <Ted_Sanders@mastercard.com>
2015-10-31 13:40:10 -0500

    username pw on db

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -35,6 +35,9 @@ environments {
 
             dbCreate = "validate"
             url = "jdbc:h2:${dbPath};MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            username = "sa"
+            password = "sa"
+
             properties {
                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                jmxEnabled = true
